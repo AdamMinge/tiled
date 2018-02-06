667d711a6dbe8155f6fc0220d9a3a73b2b9ec20d
Adam Minge <adamin@st.amu.edu.pl>
2018-02-06 16:39:45 +0100

    Added TilesetEditor

---
+++ Tiled/tileseteditor.cpp
@@ -0,0 +1,73 @@
+﻿/*-----------------------------------------------------------------------------------------------------------*/
+#include <QMainWindow>
+#include "undodock.h"
+#include "tileseteditor.h"
+#include "tilesetdocument.h"
+#include "propertiesdock.h"
+#include "animationcreatordialog.h"
+#include "preferencesmanager.h"
+/*-----------------------------------------------------------------------------------------------------------*/
+TilesetEditor::TilesetEditor() :
+	mCurrentTilesetDocument(nullptr),
+	mMainWindow(new QMainWindow),
+	mUndoDock(new UndoDock(mMainWindow.data())),
+	mPropertiesDock(new PropertiesDock(mMainWindow.data())),
+	mAnimationCreator(new AnimationCreatorDialog(mMainWindow.data()))
+{
+	mMainWindow->setDockOptions(mMainWindow->dockOptions() | QMainWindow::GroupedDragging);
+	mMainWindow->setDockNestingEnabled(true);
+
+	mMainWindow->addDockWidget(Qt::LeftDockWidgetArea, mUndoDock);
+	mMainWindow->addDockWidget(Qt::LeftDockWidgetArea, mPropertiesDock);
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+void TilesetEditor::setCurrentDocument(Document* document)
+{
+	auto tilesetDocument = qobject_cast<TilesetDocument*>(document);
+	Q_ASSERT(tilesetDocument || !document);
+
+	if (mCurrentTilesetDocument == document) return;
+
+	mCurrentTilesetDocument = tilesetDocument;
+
+	mUndoDock->setStack(document ? document->undoStack() : nullptr);
+	mPropertiesDock->setDocument(document);
+	mAnimationCreator->setTilesetDocument(tilesetDocument);
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+Document* TilesetEditor::currentDocument() const
+{
+	return mCurrentTilesetDocument;
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+QWidget* TilesetEditor::editorWidget() const
+{
+	Q_ASSERT(mMainWindow);
+	return mMainWindow.data();
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+void TilesetEditor::saveState()
+{
+	PreferencesManager::instance()->setTilesetEditorState(mMainWindow->saveState());
+	PreferencesManager::instance()->setTilesetEditorSize(mMainWindow->size());
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+void TilesetEditor::restoreState()
+{
+	auto state = PreferencesManager::instance()->tilesetEditorState();
+	auto size = PreferencesManager::instance()->tilesetEditorSize();
+
+	if (!size.isEmpty()) mMainWindow->resize(size);
+	if (!state.isEmpty()) mMainWindow->restoreState(state);
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+QList<QDockWidget*> TilesetEditor::dockWidgets() const
+{
+	return QList<QDockWidget*>{ mUndoDock, mPropertiesDock };
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+QList<DialogWithToggleView*> TilesetEditor::dialogWidgets() const
+{
+	return QList<DialogWithToggleView*>{ mAnimationCreator };
+}
+/*-----------------------------------------------------------------------------------------------------------*/
---
+++ Tiled/tileseteditor.h
@@ -0,0 +1,38 @@
+﻿/*-----------------------------------------------------------------------------------------------------------*/
+#pragma once
+/*-----------------------------------------------------------------------------------------------------------*/
+#include "editor.h"
+/*-----------------------------------------------------------------------------------------------------------*/
+class QMainWindow;
+class UndoDock;
+class PropertiesDock;
+class TilesetDocument;
+class AnimationCreatorDialog;
+/*-----------------------------------------------------------------------------------------------------------*/
+class TilesetEditor final : public Editor
+{
+public:
+	explicit TilesetEditor();
+	~TilesetEditor() override = default;
+
+	void setCurrentDocument(Document* document) override;
+	Document* currentDocument() const override;
+
+	QWidget *editorWidget() const override;
+
+	void saveState() override;
+	void restoreState() override;
+
+	QList<QDockWidget*> dockWidgets() const override;
+	QList<DialogWithToggleView*> dialogWidgets() const override;
+
+private:
+	TilesetDocument * mCurrentTilesetDocument;
+	QScopedPointer<QMainWindow> mMainWindow;
+
+	UndoDock* mUndoDock;
+	PropertiesDock* mPropertiesDock;
+
+	AnimationCreatorDialog* mAnimationCreator;
+};
+/*-----------------------------------------------------------------------------------------------------------*/

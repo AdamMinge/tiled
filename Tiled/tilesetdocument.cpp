44d965f329a7a5af9fe73c08e1f6a3c38308e70b
Adam Minge <adamin@st.amu.edu.pl>
2018-02-06 16:40:06 +0100

    Added TilesetDocument

---
+++ Tiled/tilesetdocument.cpp
@@ -0,0 +1,41 @@
+﻿/*-----------------------------------------------------------------------------------------------------------*/
+#include <QFileInfo>
+#include "tileset.h"
+#include "tilesetdocument.h"
+/*-----------------------------------------------------------------------------------------------------------*/
+TilesetDocument::TilesetDocument(Tileset* tileset, const QString& fileName) :
+	Document(fileName, createDisplayName(fileName, tileset), DocumentType::TilesetDocumentType),
+	mTileset(tileset),
+	mCurrentTile(nullptr)
+{
+
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+Tileset* TilesetDocument::tileset() const
+{
+	return mTileset;
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+void TilesetDocument::setCurrentTile(Tile* tile)
+{
+	if (mCurrentTile == tile) return;
+
+	mCurrentTile = tile;
+	emit currentTileChanged(mCurrentTile);
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+Tile* TilesetDocument::currentTile() const
+{
+	return mCurrentTile;
+}
+/*-----------------------------------------------------------------------------------------------------------*/
+QString TilesetDocument::createDisplayName(const QString& fileName, Tileset* tileset)
+{
+	Q_ASSERT(tileset);
+
+	auto displayName = QFileInfo(fileName).fileName();
+
+	if (displayName.isEmpty()) return tileset->name();
+	else return displayName;
+}
+/*-----------------------------------------------------------------------------------------------------------*/
---
+++ Tiled/tilesetdocument.h
@@ -0,0 +1,36 @@
+﻿/*-----------------------------------------------------------------------------------------------------------*/
+#pragma once
+/*-----------------------------------------------------------------------------------------------------------*/
+#include "document.h"
+/*-----------------------------------------------------------------------------------------------------------*/
+class Tileset;
+class Tile;
+/*-----------------------------------------------------------------------------------------------------------*/
+class TilesetDocument final : public Document
+{
+	Q_OBJECT
+
+public:
+	explicit TilesetDocument(Tileset* tileset, const QString &fileName = QString());
+	~TilesetDocument() override = default;
+
+	Tileset* tileset() const;
+
+	void setCurrentTile(Tile* tile);
+	Tile* currentTile() const;
+
+signals:
+	void currentTileChanged(Tile* tile);
+
+	void tileChanged(Tile* tile, int changedPropertyId);
+	void tilesetChanged(Tileset* tileset, int changedPropertyId);
+
+private:
+	static QString createDisplayName(const QString& fileName, Tileset* tileset);
+
+private:
+	Tileset* mTileset;
+
+	Tile* mCurrentTile;
+};
+/*-----------------------------------------------------------------------------------------------------------*/
